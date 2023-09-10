7ae74e3b2978fcea3b0ef6883f9f347291415d96
Sheila Putri Javana <jvnsheila@gmail.com>
2023-09-10 19:23:35 +0700

    refactor: url builder options

--- src/routes/api/hero/+server.ts
+++ src/routes/api/hero/+server.ts
@@ -3,15 +3,15 @@ import type { Hero } from '$lib/types';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET = (async ({ fetch }) => {
-	const params = new URLSearchParams({ language: 'english' });
-	const response = await fetch(urlBuilder('herolist', { params }));
+  const params = { language: 'english' };
+  const response = await fetch(urlBuilder('herolist', { params }));
 
-	const data = await response.json();
-	const heroes = data.result.data.heroes as Array<Hero>;
+  const data = await response.json();
+  const heroes = data.result.data.heroes as Array<Hero>;
 
-	return json({
-		heroes
-	});
+  return json({
+    heroes,
+  });
 }) satisfies RequestHandler;
 
 export const prerender = true;
