8066832d1759794c6a9a672bd405403547db9ea0
Erick Christian <erickchristian48@gmail.com>
2023-09-25 08:17:15 +0700

    feat: attribute switch component

--- src/routes/randomizer/+page.svelte
+++ src/routes/randomizer/+page.svelte
@@ -4,6 +4,7 @@
   import image from '$lib/images/background.jpg';
   import type { Hero } from '$lib/types';
   import type { PageData } from './$types';
+  import AttributeSwitch from './AttributeSwitch.svelte';
 
   export let data: PageData;
 
@@ -15,11 +16,11 @@
   class="min-h-screen w-screen bg-cover bg-top bg-no-repeat"
 >
   <section class="mx-auto max-w-4xl text-white">
-    <h1 class="text-center font-serif text-6xl font-bold drop-shadow">
+    <h1 class="pt-24 text-center font-serif text-6xl font-bold drop-shadow">
       RANDOMIZE YOUR HERO
     </h1>
 
-    <p class="text-center text-2xl text-gray-300">
+    <p class="mt-4 text-center text-2xl text-gray-300">
       From magical tacticians to fierce brutes and cunning rogues, Dota 2's hero
       pool is massive and limitlessly diverse. Unleash incredible abilities and
       devastating ultimates on your way to victory.
@@ -31,11 +32,30 @@
       {JSON.stringify(randomHero, null, 2)}
     </h2>
 
-    <button
-      class="highlight bg-gradient-to-b from-hippie-green-500 to-hippie-green-600 px-20 py-3 font-bold uppercase tracking-widest shadow-blue-950 transition-all hover:brightness-110 active:brightness-125"
-      on:click={() => (randomHero = getRandomHero(data.heroes))}
-      ><span class="drop-shadow">New Hero</span></button
+    <form
+      on:submit={(e) => {
+        console.log(e);
+        const formdata = new FormData(e.target);
+        const tono = Array.from(formdata.values());
+
+        console.log({ formdata });
+        console.log(formdata.get('attribute'));
+      }}
     >
+      <div class="flex [&>button]:mr-[-12px]">
+        <AttributeSwitch attribute={0} />
+        <AttributeSwitch attribute={1} />
+        <AttributeSwitch attribute={2} />
+        <AttributeSwitch attribute={3} />
+      </div>
+
+      <button
+        class="highlight bg-gradient-to-b from-hippie-green-500 to-hippie-green-600 px-20 py-3 font-bold uppercase tracking-widest shadow-blue-950 transition-all hover:brightness-110 active:brightness-125"
+        type="submit"
+        on:click={() => (randomHero = getRandomHero(data.heroes))}
+        ><span class="drop-shadow">New Hero</span></button
+      >
+    </form>
   </section>
 </main>
 
---
+++ src/routes/randomizer/AttributeSwitch.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import agi from '$lib/images/filters/agi.webp';
+  import int from '$lib/images/filters/int.webp';
+  import str from '$lib/images/filters/str.webp';
+  import uni from '$lib/images/filters/uni.webp';
+  import type { Hero } from '$lib/types';
+
+  export let attribute: Hero['primary_attr'];
+
+  const assets = [str, agi, int, uni];
+  $: src = assets[attribute];
+  $: id = `attr-switch-${attribute}`;
+  $: label = `Attribute ${attribute}`;
+
+  let checked = false;
+</script>
+
+<label
+  for={id}
+  class="cursor-pointer transition-all"
+  class:brightness-50={checked}
+  class:saturate-0={checked}
+>
+  <img {src} alt={label} />
+  <input
+    type="checkbox"
+    name="attribute"
+    {id}
+    aria-label={label}
+    value={attribute}
+    class="hidden"
+    bind:checked
+  />
+</label>
